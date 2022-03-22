c459769d4aa0f9b56d2f333cde70cfb8353fcbab
wandieinnocents <wandieinnocent2@gmail.com>
2022-03-22 16:54:28 +0300

    Add page titles

--- resources/views/frontend/pages_frontend/contact/create.blade.php
+++ resources/views/frontend/pages_frontend/contact/create.blade.php
@@ -2,6 +2,8 @@
 
 @section('title')
 
+Support 
+
 @endsection
 
 @section('content')
--- resources/views/frontend/pages_frontend/resources/show.blade.php
+++ resources/views/frontend/pages_frontend/resources/show.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts_frontend.master')
 
 @section('title')
-
+View Resource
 @endsection
 
 @section('content')

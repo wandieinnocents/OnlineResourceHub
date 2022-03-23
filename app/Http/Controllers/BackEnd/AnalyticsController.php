9d8fae9383c276a5dc317e9f56fe58a21334891b
wandieinnocents <wandieinnocent2@gmail.com>
2022-03-23 21:47:57 +0300

    Modify resouce caregories

--- app/Http/Controllers/BackEnd/AnalyticsController.php
+++ app/Http/Controllers/BackEnd/AnalyticsController.php
@@ -13,6 +13,7 @@ class AnalyticsController extends Controller
     //analytics
     public function analytics(){
 
+        
         // fetch visitors and page views
         $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(5));
         // fetch popular pages
@@ -23,7 +24,7 @@ class AnalyticsController extends Controller
         $fetch_top_browsers = Analytics::fetchTopBrowsers(Period::days(365));
         // fetch top countries
         // $fetch_top_countries = Analytics::fetchCountries(Period::days(365));
-
+        
         // dd($fetch_top_countries);
         // $analyticsData = Analytics::performQuery(Period::years(1), 'ga:sessions',['metrics' => 'ga:sessions, ga:pageviews','dimensions' => 'ga:country']);
         // dd($analyticsData); 

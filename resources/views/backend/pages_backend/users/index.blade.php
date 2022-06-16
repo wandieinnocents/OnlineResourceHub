ded2bfc097fee4fa6a050002e683ede55a1d793d
wandieinnocents <wandieinnocent2@gmail.com>
2022-06-16 22:02:05 +0300

    Refactor code

--- resources/views/backend/pages_backend/users/index.blade.php
+++ resources/views/backend/pages_backend/users/index.blade.php
@@ -18,7 +18,7 @@ All Users
         <div class="row align-items-center">
             <div class="col-md-6">
                 <div class="mb-3">
-                    <h5 class="card-title">Users <span class="text-muted fw-normal ms-2">88</span></h5>
+                    <!-- <h5 class="card-title">Users <span class="text-muted fw-normal ms-2">88</span></h5> -->
                 </div>
             </div>
 

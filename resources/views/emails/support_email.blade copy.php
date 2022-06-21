dba5a9009bc2a48737142e2a27b345e9d226b77b
wandieinnocents <wandieinnocent2@gmail.com>
2022-06-21 16:43:29 +0300

    Refactor mails

---
+++ resources/views/emails/support_email.blade copy.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>SENSE ONLINE WEB PORTAL</title>
+
+</head>
+
+<body>
+    <h3>FEEDBACK MESSAGE </h3>
+    <p>Full Name : {{ $details['name'] }}</p>
+    <p> Subject   : {{ $details['subject'] }}</p>
+    <p>Phone No   : {{ $details['phone'] }}</p>
+    <p>Email      : {{ $details['email'] }}</p>
+    <p>Message    : {{ $details['description'] }}</p>
+
+
+
+    <p>Thank you</p>
+
+</body>
+
+</html>
--- resources/views/emails/support_email.blade.php
+++ resources/views/emails/support_email.blade.php
@@ -5,18 +5,70 @@
     <title>SENSE ONLINE WEB PORTAL</title>
 
 </head>
-
 <body>
-    <h3>FEEDBACK MESSAGE </h3>
-    <p>Full Name : {{ $details['name'] }}</p>
-    <p> Subject   : {{ $details['subject'] }}</p>
-    <p>Phone No   : {{ $details['phone'] }}</p>
-    <p>Email      : {{ $details['email'] }}</p>
-    <p>Message    : {{ $details['description'] }}</p>
+    <!-- BODY -->
+    <table class="body-wrap">
+        <tr>
+            <td></td>
+            <td class="container" style="background-color:#FFFFFF">
+
+                <div class="content">
+                    <table>
+                        <tr>
+                            <td>
+
+                                <center>
+                                    <h3>Hi, Administrator</h3>
+                                </center>
+
+                                <table style="margin-bottom:20px">
+                                    <tr>
+                                        <td>
+                                            <div class="callout" style="text-align:center;margin:0 auto">
+                                                <h3><b>You have an email from</b></h3>
+                                                <div class="callout" style="text-align:center;margin:0 auto">
+                                                    <h4>{{ $details['name'] }}</h4>
+                                                    <p>Subject : {{ $details['subject'] }}</p>
+                                                    <p>Phone : {{ $details['phone'] }}</p>
+
+                                                </div>
+                                        </td>
+                                    </tr>
+                                </table>
+
+                                <p style="text-align:left"> Message, </p>
+
+                                <table class="social" width="100%">
+                                    <tr>
+                                        <td>
+
+                                            <table style="padding:15px;">
+                                                <tr>
+                                                    <td>
+                                                        <p
+                                                            style="font-size:12px !important;border-bottom:1px solid #ccc;padding-bottom:15px">
+                                                            {{ $details['description'] }}
+
+                                                        </p>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
 
 
+                                        </td>
+                                    </tr>
+                                </table>
+                </div>
 
-    <p>Thank you</p>
+            </td>
+
+        </tr>
+    </table><!-- /BODY -->
+
 
 </body>
 
